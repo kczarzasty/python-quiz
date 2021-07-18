e90d8e3634ccd91afcf4ffdfd73b9cc465cf2dd5
Kevin Czarzasty <82130684+kczarzasty@users.noreply.github.com>
2021-07-18 14:51:24 -0400

    Update and rename Main.tf
    Rename main.tf to question-data.tf
    Update question data

---
+++ question-data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "What is the full name of the person who created the Linux kernel?", "answer": "Linus Torvalds"},
+    {"text": "What is the Linux command for making a new directory?", "answer": "mkdir"},
+    {"text": "What is the Linux command to concatenate the contents of a file", "answer": "cat"},
+    {"text": "What is the year when Bash was first released?", "answer": "1989"},
+    {"text": "aaaaaaaaaaaaa", "answer": "aa"},
+    {"text": "aaaaaaaaaaaaa", "answer": "aa"},
+    {"text": "aaaaaaaaaaaaa", "answer": "aa"},
+    {"text": "aaaaaaaaaaaaa", "answer": "aa"},
+    {"text": "aaaaaaaaaaaaa", "answer": "aa"},
+    {"text": "aaaaaaaaaaaaa", "answer": "aa"},
+    {"text": "aaaaaaaaaaaaa", "answer": "aa"},
+    {"text": "aaaaaaaaaaaaa", "answer": "aa"}
+]
