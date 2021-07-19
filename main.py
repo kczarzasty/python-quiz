c5fc771fd526183b693f9c00736a8b75aeac31c2
Kevin Czarzasty <82130684+kczarzasty@users.noreply.github.com>
2021-07-19 00:21:26 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
