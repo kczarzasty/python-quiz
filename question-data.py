f9aed1822bafd9c4828d81cfe3f952d9fffaa8e9
Kevin Czarzasty <82130684+kczarzasty@users.noreply.github.com>
2021-07-20 00:55:10 -0400

    Update question-data.py

--- question-data.py
+++ question-data.py
@@ -1,12 +1,12 @@
 question_data = [
-    {"text": "What is the full name of the person who created the Linux kernel?", "answer": "Linus Torvalds"},
+    {"text": "What is the first name of the person who created the Linux kernel?", "answer": "Linus"},
     {"text": "What is the Linux command for making a new directory?", "answer": "mkdir"},
-    {"text": "What is the Linux command to concatenate the contents of a file", "answer": "cat"},
-    {"text": "What is the year when Bash was first released?", "answer": "1989"},
+    {"text": "What is the Linux command for concatenating contents of a file", "answer": "cat"},
+    {"text": "In which year was Bash was first released?", "answer": "1989"},
     {"text": "Fill in the blank: Bash stands for _______ Again Shell", "answer": "Bourne"},
-    {"text": "What is the Linux command to print the last ten lines of a text file?", "answer": "tail"},
-    {"text": "The founder of Linux also authored this software to track file changes", "answer": "Git"},
+    {"text": "What is the Linux command for printing the last ten lines of a text file?", "answer": "tail"},
+    {"text": "The founder of Linux also authored this software to track file changes.", "answer": "Git"},
     {"text": "Which Linux command allows you to move and rename files and directories?", "answer": "mv"},
     {"text": "Which Linux command displays the current user's username?", "answer": "whoami"},
-    {"text": "aaaaaaaaaaaaa", "answer": "aa"},
+    {"text": "True or False: Bash's logo is a Penguin.", "answer": "False"},
 ]
