6350e758189308cf920c96c47a495208fa25ff98
Kevin Czarzasty <82130684+kczarzasty@users.noreply.github.com>
2021-07-23 13:28:21 -0400

    Update question-data.py

--- question-data.py
+++ question-data.py
@@ -3,7 +3,7 @@ question_data = [
     {"text": "What is the first name of the person who created the Linux kernel?", "answer": "Linus"},
     {"text": "What is the Linux command for making a new directory?", "answer": "mkdir"},
     {"text": "What is the Linux command for concatenating contents of a file", "answer": "cat"},
-    {"text": "In which year was Bash was first released?", "answer": "1989"},
+    {"text": "In which year was Bash first released?", "answer": "1989"},
     {"text": "Fill in the blank: Bash stands for _______ Again Shell", "answer": "Bourne"},
     {"text": "What is the Linux command for printing the last ten lines of a text file?", "answer": "tail"},
     {"text": "The founder of Linux also authored this software to track file changes.", "answer": "Git"},
