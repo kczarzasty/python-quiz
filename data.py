0feb0794084c1003784de6d7127f1ed591311f71
Kevin Czarzasty <82130684+kczarzasty@users.noreply.github.com>
2021-07-28 13:29:30 -0400

    Update and rename question-data.py to data.py

---
+++ data.py
@@ -0,0 +1,13 @@
+# questions to be asked of the quiz taker
+question_data = [
+    {"question": "Linus is the first name of the person who created the Linux kernel.", "answer": "True"},
+    {"question": "mkdir is the Linux command for making a new directory.", "answer": "True"},
+    {"question": "con is the Linux command for concatenating contents of a file", "answer": "False"},
+    {"question": "Bash was first released in 1989.", "answer": "True"},
+    {"question": "Bash stands for Bourne Again Shell", "answer": "True"},
+    {"question": "head is the Linux command for printing the last ten lines of a text file.", "answer": "False"},
+    {"question": "The founder of Linux also authored Git software to track file changes.", "answer": "True"},
+    {"question": "mv command allows you to move and rename files and directories.", "answer": "True"},
+    {"question": "whatami command displays the current user's username", "answer": "False"},
+    {"question": "True or False: Bash's logo is a Penguin.", "answer": "False"},
+]
